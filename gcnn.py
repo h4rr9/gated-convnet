63afca9cb160f87e672e3060039e29990533611d
Hariganesh Srinivasan <hariganeshs1999@gmail.com>
2019-05-17 00:29:43 +0530

    minor bug fixes

--- gcnn.py
+++ gcnn.py
@@ -4,15 +4,29 @@ import utils
 import os
 import layers
 
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
-PATH_CHECKPOINTS = './checkpoints'
-# PATH_CHECKPOINTS = '/scratch/scratch5/harig/gcnn_sub/checkpoints'
-PATH_GRAPHS = './graphs'
-# PATH_GRAPHS = '/scratch/scratch5/harig/gcnn_sub/graphs'
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
+PATH_CHECKPOINTS = "./checkpoints"
+# PATH_CHECKPOINTS = '/scratch/scratch5/harig/tcNet_sub/checkpoints'
+PATH_GRAPHS = "./graphs"
+# PATH_GRAPHS = '/scratch/scratch5/harig/tcNet_sub/graphs'
+
+PATH_BEST_WEIGHTS = "./best_weights"
+
+DATASET = "sub"
 
 config = tf.ConfigProto()
 config.intra_op_parallelism_threads = 4
 config.inter_op_parallelism_threads = 4
+
+if DATASET == "sub":
+    load_data = utils.load_subjectivity_data
+    load_embeddings = utils.load_embeddings_subjectivity
+elif DATASET == "pol":
+    load_data = utils.load_polarity_data
+    load_embeddings = utils.load_embeddings_polarity
+elif DATASET == "sst2":
+    load_data = utils.load_sst2_data
+    load_embeddings = utils.load_embeddings_sst2
 
 
 class GatedConvNet:
@@ -21,20 +35,33 @@ class GatedConvNet:
         self.batch_size = 50
         self.learning_rate = 1
         self.l2_constraint = 3
-        self.gstep = tf.get_variable('global_step',
-                                     initializer=tf.constant_initializer(0),
-                                     dtype=tf.int32, trainable=False, shape=[])
-        self.vstep = tf.get_variable('validation_step',
-                                     initializer=tf.constant_initializer(0),
-                                     dtype=tf.int32, trainable=False, shape=[])
         self.n_classes = 2
         self.skip_step = 20
         self.training = False
-        self.keep_prob = 0.5
         self.best_acc = 0
 
+    def init(self):
+        self.train_prob = 0.5
+        self.test_prob = 1.0
+        self.keep_prob = tf.placeholder(name="keep_prob", shape=[], dtype=tf.float32)
+
+        self.gstep = tf.get_variable(
+            "global_step",
+            initializer=tf.constant_initializer(0),
+            dtype=tf.int32,
+            trainable=False,
+            shape=[],
+        )
+        self.vstep = tf.get_variable(
+            "validation_step",
+            initializer=tf.constant_initializer(0),
+            dtype=tf.int32,
+            trainable=False,
+            shape=[],
+        )
+
     def import_data(self):
-        train, val = utils.load_subjectivity_data()
+        train, val = load_data()
 
         self.max_sentence_size = train[0].shape[1]
         self.n_train = train[0].shape[0]
@@ -48,97 +75,120 @@ class GatedConvNet:
         val_data = val_data.batch(self.batch_size)
 
         iterator = tf.data.Iterator.from_structure(
-            train_data.output_types, train_data.output_shapes)
+            train_data.output_types, train_data.output_shapes
+        )
 
         self.sentence, self.label = iterator.get_next()
 
         self.train_init = iterator.make_initializer(train_data)
         self.val_init = iterator.make_initializer(val_data)
 
-    def get_embedding(self):
-        with tf.name_scope('embed'):
-            embedding_matrix = utils.load_embeddings_subjectivity()
-            _embed = tf.constant(embedding_matrix)
-            embed_matrix = tf.get_variable(
-                'embed_matrix',
-                initializer=_embed)
+    def get_embedding(self, _weights=None):
+        with tf.name_scope("embed"):
+
+            if _weights is None:
+                embedding_matrix = load_embeddings()
+                _embed = tf.constant(embedding_matrix)
+                embed_matrix = tf.get_variable("embed_matrix", initializer=_embed)
+            else:
+                _embed = tf.constant(_weights)
+                embed_matrix = tf.get_variable("embed_matrix", initializer=_embed)
 
             self.embed = tf.nn.embedding_lookup(
-                embed_matrix, self.sentence, name='embed_lookup')
+                embed_matrix, self.sentence, name="embed"
+            )
 
     def model(self):
         block0 = layers.gate_block(
-            inputs=self.embed, k_size=7, filters=100, scope_name='block0')
+            inputs=self.embed, k_size=3, filters=100, scope_name='block0')
 
         pool0 = layers.one_maxpool(
-            inputs=block0, stride=1, padding='VALID', scope_name='pool0')
+            inputs=block0, padding='VALID', scope_name='pool0')
 
         flatten0 = layers.flatten(pool0, scope_name='flatten0')
 
-        dropout0 = layers.Dropout(
-            inputs=flatten0, rate=1 - self.keep_prob, scope_name='dropout0')
+        block1 = layers.gate_block(
+            inputs=self.embed, k_size=4, filters=100, scope_name='block1')
 
-        self.logits_train = layers.fully_connected(
+        pool1 = layers.one_maxpool(
+            inputs=block1, padding='VALID', scope_name='pool1')
+
+        flatten1 = layers.flatten(pool1, scope_name='flatten1')
+
+        block2 = layers.gate_block(
+            inputs=self.embed, k_size=5, filters=100, scope_name='block2')
+
+        pool2 = layers.one_maxpool(
+            inputs=block2, padding='VALID', scope_name='pool2')
+
+        flatten2 = layers.flatten(pool2, scope_name='flatten2')
+
+        concat0 = layers.concatinate(
+            inputs=[flatten0, flatten1, flatten2], scope_name='concat0')
+
+        dropout0 = layers.Dropout(
+            inputs=concat0, rate=1 - self.keep_prob, scope_name='dropout0')
+
+        self.logits = layers.fully_connected(
             inputs=dropout0, out_dim=self.n_classes, scope_name='fc0')
 
-        self.logits_test = layers.fully_connected(
-            inputs=flatten0, out_dim=self.n_classes, scope_name='fc0')
 
-    def loss(self):
-        with tf.name_scope('loss'):
+    def init_loss(self):
+        with tf.name_scope("loss"):
             entropy = tf.nn.softmax_cross_entropy_with_logits(
-                labels=self.label, logits=self.logits_train)
-            loss = tf.reduce_mean(entropy, name='loss')
+                labels=self.label, logits=self.logits
+            )
+            loss = tf.reduce_mean(entropy, name="loss")
 
-            vars = [v for v in tf.trainable_variables() if 'fc' in v.name]
+            vars = [v for v in tf.trainable_variables() if "fc" in v.name]
 
             l2_norm = tf.add_n([tf.nn.l2_loss(v) for v in vars])
-            self.loss = loss + self.l2_constraint * l2_norm
+            self.loss = loss + (self.l2_constraint / (2 * self.batch_size))* l2_norm
 
-    def optimize(self):
-        with tf.name_scope('optimize'):
-            _opt = tf.train.AdadeltaOptimizer(
-                learning_rate=self.learning_rate)
+    def init_optimize(self):
+        with tf.name_scope("optimize"):
+            _opt = tf.train.AdadeltaOptimizer(learning_rate=self.learning_rate)
             self.opt = _opt.minimize(self.loss, global_step=self.gstep)
 
-    def summaries(self):
-        with tf.name_scope('train_summaries'):
-            train_loss = tf.summary.scalar('train_loss', self.loss)
+    def init_summaries(self):
+        with tf.name_scope("train_summaries"):
+            train_loss = tf.summary.scalar("train_loss", self.loss)
             train_accuracy = tf.summary.scalar(
-                'train_accuracy', self.accuracy / self.batch_size)
-            hist_train_loss = tf.summary.histogram(
-                'histogram_train_loss', self.loss)
+                "train_accuracy", self.accuracy / self.batch_size
+            )
+            hist_train_loss = tf.summary.histogram("histogram_train_loss", self.loss)
             self.train_summary_op = tf.summary.merge(
-                [train_loss, train_accuracy, hist_train_loss])
+                [train_loss, train_accuracy, hist_train_loss]
+            )
 
-        with tf.name_scope('val_summaries'):
-            val_loss = tf.summary.scalar('val_loss', self.loss)
+        with tf.name_scope("val_summaries"):
+            val_loss = tf.summary.scalar("val_loss", self.loss)
             val_summary = tf.summary.scalar(
-                'val_accuracy', self.accuracy / self.batch_size)
-            hist_val_loss = tf.summary.histogram(
-                'histogram_val_loss', self.loss)
+                "val_accuracy", self.accuracy / self.batch_size
+            )
+            hist_val_loss = tf.summary.histogram("histogram_val_loss", self.loss)
             self.val_summary_op = tf.summary.merge(
-                [val_loss, val_summary, hist_val_loss])
+                [val_loss, val_summary, hist_val_loss]
+            )
 
-    def eval(self):
-        with tf.name_scope('eval'):
-            preds = tf.nn.softmax(self.logits_test)
-            correct_preds = tf.equal(
-                tf.argmax(preds, 1), tf.argmax(self.label, 1))
+    def init_eval(self):
+        with tf.name_scope("eval"):
+            preds = tf.nn.softmax(self.logits)
+            correct_preds = tf.equal(tf.argmax(preds, 1), tf.argmax(self.label, 1))
             self.accuracy = tf.reduce_sum(tf.cast(correct_preds, tf.float32))
 
-            self.increment_vstep = tf.assign_add(
-                self.vstep, 1, name='increment_vstep')
+            self.increment_vstep = tf.assign_add(self.vstep, 1, name="increment_vstep")
 
     def build(self):
 
+        self.init()
         self.import_data()
         self.get_embedding()
         self.model()
-        self.loss()
-        self.optimize()
-        self.eval()
-        self.summaries()
+        self.init_loss()
+        self.init_optimize()
+        self.init_eval()
+        self.init_summaries()
 
     def train_one_epoch(self, sess, init, writer, epoch, step):
         start_time = time.time()
@@ -151,14 +201,13 @@ class GatedConvNet:
         try:
             while True:
                 _, l, accuracy_batch, summaries = sess.run(
-                    [self.opt,
-                     self.loss,
-                     self.accuracy,
-                     self.train_summary_op])
+                    [self.opt, self.loss, self.accuracy, self.train_summary_op],
+                    feed_dict={self.keep_prob: self.train_prob},
+                )
                 writer.add_summary(summaries, global_step=step)
 
                 if (step + 1) % self.skip_step == 0:
-                    print('Loss at step {0}: {1}'.format(step, l))
+                    print("Loss at step {0}: {1}".format(step, l))
                 step = step + 1
                 total_correct_preds = total_correct_preds + accuracy_batch
                 total_loss = total_loss + l
@@ -166,11 +215,17 @@ class GatedConvNet:
         except tf.errors.OutOfRangeError:
             pass
 
-        print('\nAverage training loss at epoch {0}: {1}'.format(
-            epoch, total_loss / n_batches))
-        print('Training accuracy at epoch {0}: {1}'.format(
-            epoch, total_correct_preds / self.n_train))
-        print('Took: {0} seconds'.format(time.time() - start_time))
+        print(
+            "\nAverage training loss at epoch {0}: {1}".format(
+                epoch, total_loss / n_batches
+            )
+        )
+        print(
+            "Training accuracy at epoch {0}: {1}".format(
+                epoch, total_correct_preds / self.n_train
+            )
+        )
+        print("Took: {0} seconds".format(time.time() - start_time))
 
         return step
 
@@ -184,7 +239,14 @@ class GatedConvNet:
         try:
             while True:
                 _, l, accuracy_batch, summaries = sess.run(
-                    [self.increment_vstep, self.loss, self.accuracy, self.val_summary_op])
+                    [
+                        self.increment_vstep,
+                        self.loss,
+                        self.accuracy,
+                        self.val_summary_op,
+                    ],
+                    feed_dict={self.keep_prob: self.test_prob},
+                )
                 writer.add_summary(summaries, global_step=val_step)
                 total_correct_preds = total_correct_preds + accuracy_batch
                 total_loss = total_loss + l
@@ -194,50 +256,63 @@ class GatedConvNet:
             pass
 
         if self.best_acc < total_correct_preds / self.n_test:
-            print('\nSaving best accuracy: {0} from {1}\n'.format(
-                total_correct_preds / self.n_test, self.best_acc))
+            print(
+                "\nSaving best accuracy: {0} from {1}\n".format(
+                    total_correct_preds / self.n_test, self.best_acc
+                )
+            )
             self.best_acc = total_correct_preds / self.n_test
-            best_saver.save(sess, PATH_CHECKPOINTS + '/best_model', self.gstep)
+            best_saver.save(sess, PATH_BEST_WEIGHTS + "/model", self.gstep)
         else:
-            print('\nBest Accuracy unchanged from : {0}\n'.format(
-                self.best_acc))
-        saver.save(sess, PATH_CHECKPOINTS + '/model', self.gstep)
+            print("\nBest Accuracy unchanged from : {0}\n".format(self.best_acc))
+        saver.save(sess, PATH_CHECKPOINTS + "/model", self.gstep)
 
-        print('Average validation loss at epoch {0}: {1}'.format(
-            epoch, total_loss / n_batches))
-        print('Validation accuracy at epoch {0}: {1}'.format(
-            epoch, total_correct_preds / self.n_test))
-        print('Took: {0} seconds\n'.format(time.time() - start_time))
+        print(
+            "Average validation loss at epoch {0}: {1}".format(
+                epoch, total_loss / n_batches
+            )
+        )
+        print(
+            "Validation accuracy at epoch {0}: {1}".format(
+                epoch, total_correct_preds / self.n_test
+            )
+        )
+        print("Took: {0} seconds\n".format(time.time() - start_time))
 
         return val_step
 
-    def train(self, n_epochs):
+    def train(self, n_epochs, resume=False):
         utils.mkdir_safe(os.path.dirname(PATH_CHECKPOINTS))
         utils.mkdir_safe(PATH_CHECKPOINTS)
+        utils.mkdir_safe(PATH_BEST_WEIGHTS)
 
         train_writer = tf.summary.FileWriter(
-            PATH_GRAPHS + '/train', tf.get_default_graph())
+            PATH_GRAPHS + "/train", tf.get_default_graph()
+        )
 
-        val_writer = tf.summary.FileWriter(
-            PATH_GRAPHS + '/val')
+        val_writer = tf.summary.FileWriter(PATH_GRAPHS + "/val")
 
         with tf.Session(config=config) as sess:
             sess.run(tf.global_variables_initializer())
             saver = tf.train.Saver()
             best_saver = tf.train.Saver(max_to_keep=1)
-            ckpt = tf.train.get_checkpoint_state(PATH_CHECKPOINTS)
 
-            if ckpt and ckpt.model_checkpoint_path:
-                saver.restore(sess, ckpt.model_checkpoint_path)
+            if resume == True:
+                ckpt = tf.train.get_checkpoint_state(PATH_CHECKPOINTS)
+
+                if ckpt and ckpt.model_checkpoint_path:
+                    saver.restore(sess, ckpt.model_checkpoint_path)
 
             step = self.gstep.eval()
             val_step = self.vstep.eval()
 
             for epoch in range(n_epochs):
                 step = self.train_one_epoch(
-                    sess, self.train_init, train_writer, epoch, step)
+                    sess, self.train_init, train_writer, epoch, step
+                )
                 val_step = self.eval_once(
-                    sess, best_saver, saver, self.val_init, val_writer, epoch, val_step)
+                    sess, best_saver, saver, self.val_init, val_writer, epoch, val_step
+                )
 
         train_writer.close()
         val_writer.close()
--- layers.py
+++ layers.py
@@ -1,26 +1,35 @@
 import tensorflow as tf
 
 
-def conv1d(inputs, filters, k_size, strides, padding, scope_name='conv1d'):
+def conv1d(inputs, filters, k_size, stride, padding, scope_name="conv", _weights=None):
     with tf.variable_scope(scope_name, reuse=tf.AUTO_REUSE) as scope:
 
         in_channels = inputs.shape[-1]
 
-        kernel = tf.get_variable('kernel', shape=[
-                                 k_size, in_channels, filters], initializer=tf.truncated_normal_initializer())
+        if _weights is None:
+            kernel = tf.get_variable(
+                "kernel",
+                [k_size, in_channels, filters],
+                initializer=tf.truncated_normal_initializer(),
+            )
 
-        biases = tf.get_variable(
-            'biases', shape=[filters], initializer=tf.random_normal_initializer())
+            biases = tf.get_variable(
+                "biases", [filters], initializer=tf.random_normal_initializer()
+            )
+        else:
+            kernel = tf.get_variable(
+                "kernel", initializer=tf.constant(_weights[0]))
 
-        conv = tf.nn.conv1d(inputs, kernel, stride=strides,
-                            padding=padding, use_cudnn_on_gpu=True)
+            biases = tf.get_variable(
+                "biases", initializer=tf.constant(_weights[1]))
+        conv = tf.nn.conv1d(inputs, kernel, stride=stride, padding=padding)
 
-        conv_output = tf.add(conv, biases, name=scope.name + '_output')
+        output = tf.add(conv, biases, name=scope.name)
 
-    return conv_output
+    return output
 
 
-def casual_conv(inputs, filters, k_size, strides, scope_name='casual_conv'):
+def casual_conv(inputs, filters, k_size, strides, scope_name='casual_conv', _weights=None):
     with tf.variable_scope(scope_name, reuse=tf.AUTO_REUSE) as scope:
         paddings = tf.constant([[0, 0], [k_size - 1, 0], [0, 0]])
 
@@ -28,15 +37,51 @@ def casual_conv(inputs, filters, k_size, strides, scope_name='casual_conv'):
             tensor=inputs, paddings=paddings, name=scope.name + '_padding')
 
         output = conv1d(inputs=padded_inputs, filters=filters,
-                        k_size=k_size, strides=strides, padding='VALID')
+                        k_size=k_size, stride=strides, padding='VALID', _weights=_weights)
 
     return output
 
 
-def sigmoid(inputs, scope_name='sigmoid'):
+def relu(inputs, scope_name="relu"):
     with tf.variable_scope(scope_name, reuse=tf.AUTO_REUSE) as scope:
-        sigma = tf.sigmoid(inputs, name=scope.name + '_output')
-    return sigma
+        _relu = tf.nn.relu(inputs, name=scope.name)
+    return _relu
+
+
+def elu(inputs, scope_name="elu"):
+    with tf.variable_scope(scope_name, reuse=tf.AUTO_REUSE) as scope:
+        _elu = tf.nn.elu(inputs, name=scope.name)
+    return _elu
+
+
+def leaky_relu(inputs, scope_name="leaky_relu"):
+    with tf.variable_scope(scope_name, reuse=tf.AUTO_REUSE) as scope:
+        _leaky_relu = tf.nn.leaky_relu(inputs, name=scope.name)
+    return _leaky_relu
+
+
+def softplus(inputs, scope_name="softplus"):
+    with tf.variable_scope(scope_name, reuse=tf.AUTO_REUSE) as scope:
+        _softplus = tf.nn.softplus(inputs, name=scope.name)
+    return _softplus
+
+
+def softsign(inputs, scope_name="softsign"):
+    with tf.variable_scope(scope_name, reuse=tf.AUTO_REUSE) as scope:
+        _softsign = tf.nn.softsign(inputs, name=scope.name)
+    return _softsign
+
+
+def sigmoid(inputs, scope_name="sigmoid"):
+    with tf.variable_scope(scope_name, reuse=tf.AUTO_REUSE) as scope:
+        _sigmoid = tf.nn.sigmoid(inputs, name=scope.name)
+    return _sigmoid
+
+
+def tanh(inputs, scope_name="tanh"):
+    with tf.variable_scope(scope_name, reuse=tf.AUTO_REUSE) as scope:
+        _tanh = tf.nn.tanh(inputs, name=scope.name)
+    return _tanh
 
 
 def gate(input_1, input_2, scope_name='gated_linear'):
@@ -46,22 +91,22 @@ def gate(input_1, input_2, scope_name='gated_linear'):
     return gate_output
 
 
-def gate_block(inputs, k_size, filters, scope_name='gate'):
+def gate_block(inputs, k_size, filters, scope_name='gate', _weights=None):
     with tf.variable_scope(scope_name, reuse=tf.AUTO_REUSE) as scope:
 
         in_channel = inputs.shape[-1]
 
         if(in_channel != filters):
             residual = conv1d(inputs, filters=filters, k_size=1,
-                              padding='VALID', strides=1, scope_name='residual_red')
+                              padding='VALID', stride=1, scope_name='residual_red', _weights=_weights)
         else:
             residual = inputs
 
         A = casual_conv(inputs=inputs, filters=filters, k_size=k_size,
-                        strides=1, scope_name='convA')
+                        strides=1, scope_name='convA', _weights=_weights)
 
         B = casual_conv(inputs=inputs, filters=filters, k_size=k_size,
-                        strides=1, scope_name='convB')
+                        strides=1, scope_name='convB', _weights=_weights)
 
         gate_block_output = gate(A, B)
 
@@ -70,30 +115,30 @@ def gate_block(inputs, k_size, filters, scope_name='gate'):
     return output
 
 
-def gate_block_b(inputs, k_size, filters, bottleneck, scope_name='gate_b'):
+def gate_block_b(inputs, k_size, filters, bottleneck, scope_name='gate_b', _weights=None):
     with tf.variable_scope(scope_name, reuse=tf.AUTO_REUSE) as scope:
 
         in_channel = inputs.shape[-1]
 
         if(in_channel != filters):
             residual = conv1d(inputs, filters=filters, k_size=1,
-                              padding='VALID', strides=1, scope_name='residual_red')
+                              padding='VALID', stride=1, scope_name='residual_red', _weights=_weights)
         else:
             residual = inputs
 
         red_inputs = conv1d(inputs, filters=bottleneck,
-                            k_size=1, strides=1, padding='VALID', scope_name='conv1d_red')
+                            k_size=1, strides=1, padding='VALID', scope_name='conv1d_red', _weights=_weights)
 
         A = casual_conv(inputs=red_inputs, filters=bottleneck, k_size=k_size,
-                        strides=1, scope_name='convA')
+                        strides=1, scope_name='convA', _weights=_weights)
 
         B = casual_conv(inputs=red_inputs, filters=bottleneck, k_size=k_size,
-                        strides=1, scope_name='convB')
+                        stride=1, scope_name='convB', _weights=_weights)
 
         gate_output = gate(A, B)
 
         gate_block_output = conv1d(
-            gate_output, filters=filters, k_size=1, strides=1, padding='VALID', scope_name='conv1d_exp')
+            gate_output, filters=filters, k_size=1, stride=1, padding='VALID', scope_name='conv1d_exp', _weights=_weights)
 
         output = tf.add(gate_block_output, residual,
                         name=scope.name + '_output')
@@ -101,34 +146,51 @@ def gate_block_b(inputs, k_size, filters, bottleneck, scope_name='gate_b'):
     return output
 
 
-def one_maxpool(inputs, stride, padding='VALID', scope_name='pool'):
+def one_maxpool(inputs, padding='VALID', scope_name='one-pool1d'):
     with tf.variable_scope(scope_name, reuse=tf.AUTO_REUSE) as scope:
 
-        _inputs = tf.expand_dims(inputs, axis=1)
-        height, width = _inputs.shape[-2:]
+        height, in_channel = inputs.shape[-2:]
 
-        _pool = tf.nn.max_pool(_inputs,
-                               ksize=[1, 1, height, 1],
-                               strides=[1, 1, stride, 1],
-                               padding=padding,
-                               name=scope.name)
-        pool = tf.squeeze(_pool, axis=1)
+        pool = tf.nn.pool(input=inputs, window_shape=[
+                          height], pooling_type='MAX', padding=padding, strides=[1], name=scope.name)
 
     return pool
 
 
-def fully_connected(inputs, out_dim, scope_name='fc'):
+def maxpool1d(inputs, k_size, stride=None, padding='VALID', scope_name='pool1d'):
     with tf.variable_scope(scope_name, reuse=tf.AUTO_REUSE) as scope:
-        in_dim = inputs.shape[-1]
 
-        w = tf.get_variable('weights',
-                            [in_dim, out_dim],
-                            initializer=tf.truncated_normal_initializer())
+        if stride is None:
+            stride = k_size
 
-        b = tf.get_variable('biases', [out_dim],
-                            initializer=tf.constant_initializer(0.0))
+        pool = tf.nn.pool(input=inputs, window_shape=[
+                          k_size], pooling_type='MAX', padding=padding, strides=[stride], name=scope.name)
 
-        out = tf.add(tf.matmul(inputs, w), b, name=scope.name + '_output')
+        return pool
+
+
+def fully_connected(inputs, out_dim, scope_name="fc", _weights=None):
+    with tf.variable_scope(scope_name, reuse=tf.AUTO_REUSE) as scope:
+
+        if _weights is None:
+            in_dim = inputs.shape[-1]
+            w = tf.get_variable(
+                "weights",
+                [in_dim, out_dim],
+                initializer=tf.truncated_normal_initializer(),
+            )
+
+            b = tf.get_variable(
+                "biases", [out_dim], initializer=tf.constant_initializer(0.0)
+            )
+        else:
+            w = tf.get_variable(
+                "weights", initializer=tf.constant(_weights[0]))
+
+            biases = tf.get_variable(
+                "biases", initializer=tf.constant(_weights[1]))
+
+        out = tf.add(tf.matmul(inputs, w), b, name=scope.name)
     return out
 
 
@@ -140,6 +202,13 @@ def flatten(inputs, scope_name='flatten'):
             inputs, shape=[-1, feature_dim], name=scope.name + '_output')
 
     return flatten
+
+
+def concatinate(inputs, scope_name):
+    with tf.variable_scope(scope_name, reuse=tf.AUTO_REUSE) as scope:
+        concat = tf.concat(inputs, 1, name=scope.name)
+
+    return concat
 
 
 def Dropout(inputs, rate, scope_name='dropout'):
